53fb69755beed144acf6520697cce08cce7b4539
hmshuv <himanshugupta54321@gmail.com>
2023-09-02 01:29:33 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// 1. Ways to print in Java Script:
+// console.log("Hello World");
+// alert("Hello");
+
+// 2. Java Script Console API
+console.warn("This is a warning");
+console.error("This is a error");
+console.log("Hello World", 4+4, "new log");
+
+// 3. JavaScript Variables 
+// Containers to store data values
+var num1 = 32;
+var num2  = 34;
+
+console.log("number1 + number2");
+
+//4. Data types in Java Sript
+//  
